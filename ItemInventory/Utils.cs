597ae80a6bd598c66a9eeb14883d9d3471e5fd96
midnightblue1412 <solar_flare252@yahoo.com.ph>
2016-02-06 16:48:17 +0800

    [modify] create class Utils

---
+++ ItemInventory/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ItemInventory
+{
+    class Utils
+    {
+        public delegate void RowProcessor(string[] cols);
+        public delegate void ErrorCallBack();
+
+        public static string[] getColumnNames(DataGridView dataGrid)
+        {
+            if (dataGrid.Columns.Count == 0)
+            {
+                return null;
+            }
+
+            string[] cols = new string[dataGrid.Columns.Count];
+
+            int i = 0;
+            foreach (DataGridViewColumn c in dataGrid.Columns)
+            {
+                cols[i] = c.Name;
+                i++;
+            }
+
+            return cols;
+        }
+
+        public static bool rowInputComplete(DataGridViewRow r)
+        {
+            DataGridViewCellCollection c = r.Cells;
+            string[] cols = getColumnNames(r.DataGridView);
+
+            foreach (string col in cols)
+            {
+                if (c[col].Value == null)
+                {
+                    r.DataGridView.CurrentCell = c[col];
+                    return false;
+                }
+            }
+
+            return true;
+        }        
+    }
+}
